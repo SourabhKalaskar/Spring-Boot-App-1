eb4df28555ae47fb2faaddd24fc985266db14390
SourabhKalaskar <sourabhkalaskar007@gmail.com>
2021-11-28 09:27:11 +0530

    main method modifed

--- src/main/java/in/ashokit/SpringBootApp01Application.java
+++ src/main/java/in/ashokit/SpringBootApp01Application.java
@@ -2,12 +2,16 @@ package in.ashokit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootApp01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApp01Application.class, args);
+	ConfigurableApplicationContext context=	SpringApplication.run(SpringBootApp01Application.class, args);
+	
+	context.close();
+	
 	}
 
 }
